13373d78c575e21aada13e08e0f5a32849af22a7
fei wang <feiwangsioc@gmail.com>
2019-11-09 23:22:13 +0000

    todolist

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,104 @@
+   
+   class Todo extends React.Component {
+     
+     constructor(props){
+        super(props);
+     
+        this.state = { done: props.done,
+                       text: props.text
+                     };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+    
+    handleClick(event){
+      this.setState(state => ({
+        done: !state.done
+      }),
+      function(event){
+        this.handleSubmit(event)
+      }
+      );
+      
+    }
+    handleChange(event){
+      let text = event.target.value;
+      
+      this.setState(state => ({
+        text: text
+      }));
+    }
+      
+    handleSubmit(event){
+      console.log("This is where the submit will happen");
+      // this.setState(state => ({
+  
+      // }));
+    }
+    
+    
+     
+     render(){
+    
+    
+       return <div classname="todo">
+              <span>
+              <input type="checkbox" checked={this.state.done} 
+                                      onClick={this.handleClick}/> 
+              <input type="text" value={this.state.text} 
+                                 onChange={this.handleChange} 
+                                 onBlur={this.handleSubmit} />
+              
+              </span>
+              </div>;
+     }
+   }
+   
+   class TodoList extends React.Component {
+     constructor(props){
+       super(props);
+       
+       this.state = { todos: [
+                               {
+                                 _id: 'a',
+                                text: 'Item 1',
+                                done: false
+                               },
+                               {
+                                 _id: 'b',
+                                text: 'Item 2',
+                                done: false
+                               },
+                               {
+                                 _id: 'c',
+                                text: 'Item 3',
+                                done: false
+                               },
+                               {
+                                 _id: 'd',
+                                text: 'Item 4',
+                                done: false
+                               },
+                               
+                             ]
+         
+                    };
+     }
+     render(){
+       const todoList = this.state.todos.map((todo) => <Todo key={todo._id.toString()} text={todo.text} done={todo.done} />);
+        
+       return <React.Fragment>
+              <h1> React Todo App </h1>
+              {todoList}
+              </React.Fragment>
+     }
+       
+     
+   }
+   
+   
     ReactDOM.render(
-      <h1>Hello, world</h1>,
-      document.getElementById('root')
-    );
+      <TodoList />, 
+      document.getElementById('root'));
